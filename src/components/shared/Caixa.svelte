07467a75ee4ee6c5137538080f030fb77575fe62
Michelle Cordeiro <mic321@gmail.com>
2024-11-19 21:30:00 -0300

    Initial commit

---
+++ src/components/shared/Caixa.svelte
@@ -0,0 +1,10 @@
+<div class="caixa"></div>
+
+<style>
+  .caixa {
+    width: 100px;
+    height: 100px;
+    background-color: rgb(53, 43, 153);
+    border: solid 2px white;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-3xl font-bold">Ordem dos Livros</h1>
+<a href="/autores" class="bg-gray-500 rounded p-2">Autores</a>
