dd839d138cd82246c8a8d1b54aadc7232df930d5
Michelle Cordeiro <mic321@gmail.com>
2024-11-20 12:33:38 -0300

    feat: navItem component

---
+++ src/components/template/NavItem.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let text = ""
+  export let href = "/"
+  export let target = ""
+</script>
+
+<li>
+  <a
+    {href}
+    {target}
+    class="
+      flex gap-2 items-center cursor-pointer px-4 py-2 text-zinc-300
+      hover:tracking-wide hover:font-bold hover:text-white hover:bg-white/10
+    "
+  >
+    <span>{text}</span>
+  </a>
+</li>
--- src/components/template/Sidebar.svelte
+++ src/components/template/Sidebar.svelte
@@ -1,21 +1,27 @@
 <script>
   import Nav from './Nav.svelte';
+  import NavItem from './NavItem.svelte';
 </script>
 
 <aside class="w-72 flex flex-col gap-10 self-start border border-zinc-800 p-5 rounded-md">
   <Nav title="Navegação">
-    <li>T1</li>
-    <li>T1</li>
-    <li>T1</li>
+    <NavItem text="Inínio" href="/" />
+    <NavItem text="Personagens" href="/" />
+    <NavItem text="Autores" href="/autores" />
+    <NavItem text="Séries" href="/" />
   </Nav>
+
   <Nav title="Principais Autores">
-    <li>T1</li>
-    <li>T1</li>
-    <li>T1</li>
+    <NavItem text="J.K. Rowling" href="/" />
+    <NavItem text="J. R.R. Tolken" href="/" />
+    <NavItem text="George R.R. Martin" href="autores/george-martin" />
   </Nav>
+
   <Nav title="Outras Categorias">
-    <li>T1</li>
-    <li>T1</li>
-    <li>T1</li>
+    <NavItem text="Últimas Notícias" href="/" />
+    <NavItem text="Séries Populares" href="/" />
+    <NavItem text="Facebook" href="www.facebook.com" target="_blank" />
+    <NavItem text="Instagram" href="www.instagram.com" target="_blank" />
+    <NavItem text="Youtube" href="www.youtube.com" target="_blank" />
   </Nav>
 </aside>
