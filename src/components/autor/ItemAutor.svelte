fde060efd9397f83e6e31ea3e51bf8831545a7bc
Michelle Cordeiro <mic321@gmail.com>
2024-11-21 18:02:37 -0300

    feat: add itemAutor component and update autores page

---
+++ src/components/autor/ItemAutor.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let autor: any = null;
+</script>
+
+{#if autor}
+  <a
+    href="/autores/{autor.id}"
+    class="
+      flex flex-col relative rounded-md overflow-hidden border border-zinc-800
+      hover:cursor-pointer
+    "
+  >
+    <img
+      src={autor.imagem}
+      alt="Foto do Autor"
+      class="
+        h-96 object-cover opacity-80
+        transition-all duration-700 ease-in-out
+        hover:opacity-100 hover:scale-110
+      "
+    />
+    <div class="absolute bottom-0 w-full flex justify-end px-6 py-4 bg-black/50">
+      <span class="text-2xl font-black">{autor.nome}</span>
+    </div>
+  </a>
+{/if}
--- src/routes/autores/+page.svelte
+++ src/routes/autores/+page.svelte
@@ -1,6 +1,11 @@
-<h1 class="text-3xl font-bold">Autores</h1>
-<div class="flex flex-col gap-2">
-  <a href="/autores/j-k-rowling">J.K. Rowling</a>
-  <a href="/autores/j-r-r-tolkien">J.R.R. Tolkein</a>
-  <a href="/autores/george-r-r-martin">George R.R. MArtin</a>
+<script>
+  import ItemAutor from '../../components/autor/ItemAutor.svelte';
+  import autores from '../../constants/autores';
+
+</script>
+<h1 class="text-3xl font-bold py-5">Autores</h1>
+<div class="grid grid-cols-2 gap-5">
+  {#each autores as autor}
+    <ItemAutor autor={autor} />
+  {/each}
 </div>
